27e0b248505840434fcb1f7b05917590d00590e0
Ali Shahidi <alishahidi1376@gmail.com>
2023-12-28 20:46:49 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {},
+		colors: {
+			white: "#ffffff",
+			primary: "#1f2278",
+			secondary: "#575ac8",
+			gray: "#999999",
+			"soft-gray": "rgba(32, 34, 120, 0.1)",
+			orange: "#f17834",
+			"soft-blue": "#b2f0fb",
+			"soft-green": "#a4fcc4",
+			"soft-red": "#f5c0be",
+			"soft-purple": "#ddc5fb",
+		},
+	},
+	plugins: [],
+};

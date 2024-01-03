61b22c1a84c48b4a4b23e2b396a865abf7394cf2
Ali Shahidi <63677039+alishahidi@users.noreply.github.com>
2024-01-02 21:38:31 -0800

    Add files via upload
    add slider

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
